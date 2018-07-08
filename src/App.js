3e655a58cde5423de501e9d8599e13e6be5d027e
dreiv <dreivoicu@gmail.com>
2018-07-08 09:29:20 +0300

    added some content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import Post from './components/Post'
 import './App.css'
 
 class App extends Component {
 	render() {
-		return <div className="App">hello world!</div>
+		return (
+			<div className="App">
+				<Post />
+			</div>
+		)
 	}
 }
 
